3db2b807b99e5de6715a3512b09db15bdaba1712
filesfm <filesfm@users.noreply.github.com>
2026-09-10 12:02:22 +0000

    deploy: 6dc74ad6da64a0cbbeb01311e449f37908458e26

--- search/all_0.js
+++ search/all_0.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['activewindowexecutablepath_0',['activeWindowExecutablePath',['../classUtilities.html#a67aaecda20210ec10c8994f7791d0c86',1,'Utilities']]],
   ['activewindowtitle_1',['activeWindowTitle',['../classUtilities.html#a347edbd6038525a6ee6089b19ee1b954',1,'Utilities']]],
-  ['autostartup_2',['autostartup',['../classMainController.html#af0d392622bd2b6f7e0b7612bd88f6b25',1,'MainController::autoStartup'],['../classSettings.html#a16004f7f538f06d72c43beb69739e62c',1,'Settings::autoStartup()'],['../classMainController.html#a477a8c106c3c977135363d253e700160',1,'MainController::autoStartup()']]],
-  ['autostartupchanged_3',['autostartupchanged',['../classSettings.html#a0bf6272b3b051efe2ebd6efbab6c573c',1,'Settings::autoStartupChanged()'],['../classMainController.html#aa653a069353e1f807ca3c660048e9b25',1,'MainController::autoStartupChanged()']]]
+  ['autostart_2',['autostart',['../classUtilities.html#a7693b4dd83127e42bc92ecd929762db6',1,'Utilities']]],
+  ['autostartup_3',['autostartup',['../classMainController.html#af0d392622bd2b6f7e0b7612bd88f6b25',1,'MainController::autoStartup'],['../classSettings.html#a16004f7f538f06d72c43beb69739e62c',1,'Settings::autoStartup()'],['../classMainController.html#a477a8c106c3c977135363d253e700160',1,'MainController::autoStartup()']]],
+  ['autostartupchanged_4',['autostartupchanged',['../classSettings.html#a0bf6272b3b051efe2ebd6efbab6c573c',1,'Settings::autoStartupChanged()'],['../classMainController.html#aa653a069353e1f807ca3c660048e9b25',1,'MainController::autoStartupChanged()']]]
 ];
