08ca39517e8c60beb0b5e8b9bbce20c0a9ae2aa6
filesfm <filesfm@users.noreply.github.com>
2026-09-25 07:55:31 +0000

    deploy: ebfd8d2a33aabec321709e73cd177980b95ac90e

--- search/functions_6.js
+++ search/functions_6.js
@@ -4,13 +4,14 @@ var searchData=
   ['sendingservice_1',['SendingService',['../classSendingService.html#a4725127232ea47a6cd1708510d5abec8',1,'SendingService']]],
   ['sendsucceeded_2',['sendSucceeded',['../classSendingService.html#a98ac1cf723a73b1d32e464c3418a5694',1,'SendingService']]],
   ['serverurl_3',['serverUrl',['../classSendingService.html#ab0196fe402ec1fcef247564586c41eaf',1,'SendingService']]],
-  ['setautostartup_4',['setautostartup',['../classSettings.html#ad7aa200f1dafa08cfc810291c7d32dc9',1,'Settings::setAutoStartup()'],['../classMainController.html#a6d95c95d127c45ed7c82b21fe8fb08fa',1,'MainController::setAutoStartup()']]],
-  ['setpassword_5',['setpassword',['../classSettings.html#aefd5ae26097e537a4ddbac179a383687',1,'Settings::setPassword()'],['../classMainController.html#aff3dd90afb1222afb2602ce81942aea6',1,'MainController::setPassword()']]],
-  ['setserverurl_6',['setServerUrl',['../classSendingService.html#a42dda7b4c7af2a3e19fa5d680219b99c',1,'SendingService']]],
-  ['setstartbuttonpushed_7',['setstartbuttonpushed',['../classSettings.html#a00dab332ef70dafde2df9eeeec793369',1,'Settings::setStartButtonPushed()'],['../classMainController.html#a4eb6f87aaf9c3bcab82ea2b288e91cda',1,'MainController::setStartButtonPushed()']]],
-  ['setusername_8',['setusername',['../classSettings.html#afe37c1523033ac62b459b7887c4164c0',1,'Settings::setUsername()'],['../classMainController.html#a62bc4d951c8437059f9ffaf1d6397c97',1,'MainController::setUsername()']]],
-  ['start_9',['start',['../classSendingService.html#a1dc0c0ecfaf50a86b735775b8eb60a0a',1,'SendingService']]],
-  ['startbuttonchanged_10',['startbuttonchanged',['../classSettings.html#abfc08a4f65f74cd346f3312f2cc7c3c4',1,'Settings::startButtonChanged()'],['../classMainController.html#a41a2e6fc4bbd8f2998da58d59b0adcaf',1,'MainController::startButtonChanged()']]],
-  ['startbuttonpushed_11',['startbuttonpushed',['../classSettings.html#ab2f834a5d54962a7ded4a316559f94b1',1,'Settings::startButtonPushed()'],['../classMainController.html#a3a2e210e17847e0c5ad3a7e39e0199c7',1,'MainController::startButtonPushed()']]],
-  ['stop_12',['stop',['../classSendingService.html#a5c9b8669459dbe85bbd290f2560af17a',1,'SendingService']]]
+  ['serverurlchanged_4',['serverUrlChanged',['../classSendingService.html#a0986f7ee849ac65afa3d20168dae6d25',1,'SendingService']]],
+  ['setautostartup_5',['setautostartup',['../classSettings.html#ad7aa200f1dafa08cfc810291c7d32dc9',1,'Settings::setAutoStartup()'],['../classMainController.html#a6d95c95d127c45ed7c82b21fe8fb08fa',1,'MainController::setAutoStartup()']]],
+  ['setpassword_6',['setpassword',['../classSettings.html#aefd5ae26097e537a4ddbac179a383687',1,'Settings::setPassword()'],['../classMainController.html#aff3dd90afb1222afb2602ce81942aea6',1,'MainController::setPassword()']]],
+  ['setserverurl_7',['setServerUrl',['../classSendingService.html#a42dda7b4c7af2a3e19fa5d680219b99c',1,'SendingService']]],
+  ['setstartbuttonpushed_8',['setstartbuttonpushed',['../classSettings.html#a00dab332ef70dafde2df9eeeec793369',1,'Settings::setStartButtonPushed()'],['../classMainController.html#a4eb6f87aaf9c3bcab82ea2b288e91cda',1,'MainController::setStartButtonPushed()']]],
+  ['setusername_9',['setusername',['../classSettings.html#afe37c1523033ac62b459b7887c4164c0',1,'Settings::setUsername()'],['../classMainController.html#a62bc4d951c8437059f9ffaf1d6397c97',1,'MainController::setUsername()']]],
+  ['start_10',['start',['../classSendingService.html#a1dc0c0ecfaf50a86b735775b8eb60a0a',1,'SendingService']]],
+  ['startbuttonchanged_11',['startbuttonchanged',['../classSettings.html#abfc08a4f65f74cd346f3312f2cc7c3c4',1,'Settings::startButtonChanged()'],['../classMainController.html#a41a2e6fc4bbd8f2998da58d59b0adcaf',1,'MainController::startButtonChanged()']]],
+  ['startbuttonpushed_12',['startbuttonpushed',['../classSettings.html#ab2f834a5d54962a7ded4a316559f94b1',1,'Settings::startButtonPushed()'],['../classMainController.html#a3a2e210e17847e0c5ad3a7e39e0199c7',1,'MainController::startButtonPushed()']]],
+  ['stop_13',['stop',['../classSendingService.html#a5c9b8669459dbe85bbd290f2560af17a',1,'SendingService']]]
 ];
