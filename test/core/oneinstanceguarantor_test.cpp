95b0c00cbbe1d0dd9e7a35cf5f0f717d26261e77
artyom-fedosov <artjomsfedosovs2@gmail.com>
2026-09-26 17:51:33 +0300

    test(oneinstanceguarantor): add general tests

---
+++ test/core/oneinstanceguarantor_test.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <type_traits>
+
+#include "core/oneinstanceguarantor.hpp"
+
+TEST(OneInstanceGuarantorTest, IsNotCopyable)
+{
+    EXPECT_FALSE(std::is_copy_constructible_v<OneInstanceGuarantor>);
+    EXPECT_FALSE(std::is_copy_assignable_v<OneInstanceGuarantor>);
+}
+
+TEST(OneInstanceGuarantorTest, FirstInstanceAcquiresLock)
+{
+    EXPECT_NO_THROW(OneInstanceGuarantor guard{});
+}
+
+TEST(OneInstanceGuarantorTest, SecondInstanceThrowsWhileFirstIsAlive)
+{
+    OneInstanceGuarantor first{};
+    EXPECT_THROW(OneInstanceGuarantor second{}, std::runtime_error);
+}
+
+TEST(OneInstanceGuarantorTest, LockIsReleasedOnDestruction)
+{
+    {
+        OneInstanceGuarantor first{};
+    }
+    EXPECT_NO_THROW(OneInstanceGuarantor second{});
+}
+
+TEST(OneInstanceGuarantorTest, ThrowsWithExpectedMessage)
+{
+    OneInstanceGuarantor first{};
+    try {
+        OneInstanceGuarantor second{};
+        FAIL();
+    } catch (const std::runtime_error &e) {
+        EXPECT_STREQ(e.what(), "One program instance already exists.");
+    }
+}
+
+TEST(OneInstanceGuarantorTest, FirstInstanceRemainsLockedAfterFailedSecondAttempt)
+{
+    OneInstanceGuarantor first{};
+
+    EXPECT_THROW(OneInstanceGuarantor second{}, std::runtime_error);
+
+    // The failed attempt above must not have disturbed first's lock.
+    EXPECT_THROW(OneInstanceGuarantor third{}, std::runtime_error);
+}
+
+TEST(OneInstanceGuarantorTest, SupportsRepeatedAcquireReleaseCycles)
+{
+    for (int i{0}; i < 5; ++i) {
+        EXPECT_NO_THROW(OneInstanceGuarantor guard{});
+    }
+}
